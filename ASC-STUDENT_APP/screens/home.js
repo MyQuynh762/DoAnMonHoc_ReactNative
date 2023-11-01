02249bb4b2c1c621debf233054832e0eb7aff135
Thuan3101 <ngominhnhut6808@gmail.com>
2023-11-01 18:22:31 +0700

    update home.js

--- ASC-STUDENT_APP/screens/home.js
+++ ASC-STUDENT_APP/screens/home.js
@@ -6,7 +6,7 @@ import NhacNho from './nhacnho';
 import DiemDanh from './diemdanh';
 import GopY from './gopy';
 import TaiKhoan from './taikhoan';
-
+import DoiMatKhau from './DoiMatKhau';
 
 function TrangChu() {
   const DATA = [
@@ -14,37 +14,37 @@ function TrangChu() {
       id: '1',
       title: 'Nhắc nhở',
       icon: require('../Icons/chuong.png'),
-      backgroundColor: 'pink',
+      backgroundColor: 'red',
     },
     {
       id: '2',
       title: 'Phiếu thu',
       icon: require('../Icons/phieuthu.png'),
-      backgroundColor: 'blue',
+      backgroundColor: '#7CFC00',
     },
     {
       id: '3',
       title: 'Công nợ',
       icon: require('../Icons/congno.png'),
-      backgroundColor: 'blue',
+      backgroundColor: '#DC143C',
     },
     {
       id: '4',
       title: 'Tin tức',
       icon: require('../Icons/tintuc.png'),
-      backgroundColor: 'blue',
+      backgroundColor: '#00FFFF',
     },
     {
       id: '5',
       title: 'Chương trình khung',
       icon: require('../Icons/chuongtrinhkhung.png'),
-      backgroundColor: 'blue',
+      backgroundColor: '#00FF00',
     },
     {
       id: '6',
       title: 'Thành tích',
       icon: require('../Icons/thanhtich.png'),
-      backgroundColor: 'blue',
+      backgroundColor: 'yellow',
     },
     {
       id: '7',
@@ -56,31 +56,32 @@ function TrangChu() {
       id: '8',
       title: 'Thống kê điểm danh',
       icon: require('../Icons/thongke.png'),
-      backgroundColor: 'blue',
+      backgroundColor: 'orange',
     },
     {
       id: '9',
       title: 'Khảo sát',
       icon: require('../Icons/khaosat.png'),
-      backgroundColor: 'blue',
+      backgroundColor: '#00008B',
     },
     {
       id: '10',
       title: 'Quảng cáo',
       icon: require('../Icons/quangcao.png'),
-      backgroundColor: 'blue',
+      backgroundColor: '#FFB6C1',
     },
     {
       id: '11',
       title: 'Mẫu đơn',
       icon: require('../Icons/maudon.png'),
-      backgroundColor: 'blue',
+      backgroundColor: '#00FFFF',
     },
     {
       id: '12',
       title: 'Đổi password',
       icon: require('../Icons/matkhau.png'),
-      backgroundColor: 'blue',
+      backgroundColor: '#FFD700',
+      screenName: 'DoiMatKhau',
     },
   ];
   
@@ -88,10 +89,12 @@ function TrangChu() {
   
   const Item = ({ title, icon, backgroundColor }) => (
     <View style={[styles.item, { backgroundColor }]}>
-      <View style={styles.iconContainer}>
-        <Image source={icon} style={styles.icon} />
+      <View style={styles.innerItem}>
+        <View style={styles.iconContainer}>
+          <Image source={icon} style={styles.icon} />
+        </View>
+        <Text style={styles.title}>{title}</Text>
       </View>
-      <Text style={styles.title}>{title}</Text>
     </View>
   );
   
@@ -99,25 +102,22 @@ function TrangChu() {
   return (
         
     <SafeAreaView style={styles.container}>
-    <View style={styles.v1}>
+      <View style={styles.v1}>
         <Pressable style={styles.bd1}></Pressable>
         <Pressable style={styles.bd2}>
-        <Image style={styles.calendar} source={require("../image/calender.PNG")}></Image>
-           <Text style={styles.txtcalendar}>Xem Lịch</Text>
-           <Image style={styles.xemdiem} source={require("../image/xemdiem.PNG")}></Image>
-           <Text style={styles.txtxemdiem}>Xem Điểm</Text>
+          <Image style={styles.calendar} source={require("../image/calender.PNG")}></Image>
+          <Text style={styles.txtcalendar}>Xem Lịch</Text>
+          <Image style={styles.xemdiem} source={require("../image/xemdiem.PNG")}></Image>
+          <Text style={styles.txtxemdiem}>Xem Điểm</Text>
         </Pressable>
-     
-    </View>
-    <FlatList
+      </View>
+      <FlatList
         numColumns={4}
         data={DATA}
-        renderItem={({ item }) => <Item title={item.title} icon={item.icon} />}
+        renderItem={({ item }) => <Item title={item.title} icon={item.icon} backgroundColor={item.backgroundColor} />}
         keyExtractor={(item) => item.id} />
-
-</SafeAreaView>
-
-);
+    </SafeAreaView>
+  );
 }
 
 
@@ -189,6 +189,7 @@ function MyTabs() {
           ),
         }}
       />
+      
     </Tab.Navigator>
   );
 }
@@ -204,25 +205,30 @@ export default function Home({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-   
+    alignItems: 'center', // Để các mục dữ liệu nằm giữa theo chiều ngang
   },
   item: {
-    backgroundColor:'#f9c2ff',
     padding: 8,
     marginVertical: 20,
     marginHorizontal: 22,
-    height: 55, // Đã tăng chiều cao để chữ nằm giữa
-    width: 55, // Đã tăng chiều rộng để chữ nằm giữa
+    height: 55,
+    width: 55,
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius:20,
+    borderRadius: 20,
+  },
+  innerItem: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   iconContainer: {
-    backgroundColor: 'transparent', // Đặt màu nền của container là trong suốt
-    width: 70, // Độ rộng của container
-    height: 70, 
-    alignItems: 'center', // Căn giữa theo chiều ngang
-    justifyContent: 'center', // Căn giữa theo chiều dọc
+    backgroundColor: 'transparent',
+    width: 70,
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     fontSize: 12,
@@ -233,53 +239,44 @@ const styles = StyleSheet.create({
   icon: {
     width: 30,
     height: 30,
-    
+    marginTop:30,
   },
-  v1:{
-
+  v1: {},
+  bd1: {
+    height: 70,
+    width: 400,
+    backgroundColor: "#047bfd",
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
   },
-  bd1:{
-    height:70,
-    width:400,
-    backgroundColor:"#047bfd",
-    borderBottomLeftRadius:50,
-    borderBottomRightRadius:50,
-  },
-  bd2:{
-    height:70,
-    width:370,
-    backgroundColor:"white",
-    borderColor:"gray",
-    borderWidth:1,
+  bd2: {
+    height: 70,
+    width: 370,
+    backgroundColor: "white",
+    borderColor: "gray",
+    borderWidth: 1,
     marginLeft: 10,
     borderRadius: 10,
-    flexDirection:"row"
-    
+    flexDirection: "row"
   },
-  calendar:{
+  calendar: {
     height: 60,
-    width:60,
+    width: 60,
     marginTop: 5,
-    marginLeft: 30
+    marginLeft: 30,
   },
-  xemdiem:{
+  xemdiem: {
     height: 60,
-    width:60,
+    width: 60,
     marginTop: 5,
-    marginLeft: 50
+    marginLeft: 50,
   },
-  txtcalendar:{
-    marginTop:25,
-    marginLeft: 10
+  txtcalendar: {
+    marginTop: 25,
+    marginLeft: 10,
   },
-  txtxemdiem:{
-    marginTop:25,
-    marginLeft: 10
+  txtxemdiem: {
+    marginTop: 25,
+    marginLeft: 10,
   },
-  
-  
-  
-   
-    
-  
 });
