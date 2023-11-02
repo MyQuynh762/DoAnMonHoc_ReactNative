26cf6f730e3b22a7b0afeab7c0ac253b7120a9d6
Thuan3101 <ngominhnhut6808@gmail.com>
2023-11-02 22:45:08 +0700

    update xem lich

--- ASC-STUDENT_APP/App.js
+++ ASC-STUDENT_APP/App.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './screens/home'; // Đảm bảo đường dẫn đúng
-import LoginScreen from './screens/loginScreen'; // Đảm bảo đường dẫn đúng
-import DoiMatKhau from './screens/DoiMatKhau'; // Đảm bảo đường dẫn đúng
-import DiemDanh from './screens2/diemdanh';
-import TrangChu from './screens2/trangchu'; // Đảm bảo đường dẫn đúng
+import Home from './screens/home';
+import LoginScreen from './screens/loginScreen'; 
+// import DoiMatKhau from './screens/DoiMatKhau'; 
+// import DiemDanh from './screens2/diemdanh';
+import TrangChu from './screens2/trangchu'; 
 import XemLich from './screens/xemlich';
 import XemDiem from './screens/xemdiem';
 
@@ -19,8 +19,8 @@ function App() {
         <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Home" component={Home}  options={{ headerShown: false }}/>
         {/* <Stack.Screen name="DoiMatKhau" component={DoiMatKhau} /> */}
-        <Stack.Screen name="TrangChu" component={TrangChu} />
-        <Stack.Screen name="XemLich" component={XemLich} />
+        <Stack.Screen name="TrangChu" component={TrangChu}options={{ headerShown: false }}  />
+        <Stack.Screen name="XemLich" component={XemLich} options={{ headerShown: false }} />
         <Stack.Screen name="XemDiem" component={XemDiem} />
       </Stack.Navigator>
     </NavigationContainer>
